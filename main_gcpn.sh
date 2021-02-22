ef34fec1530dc5b44d6ede4324dd6ed8cd3e8143
Yulun Rayn Wu <yulun_wu@berkeley.edu>
2021-02-22 04:07:21 -0800

    fix setup

--- main_gcpn.sh
+++ main_gcpn.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 eval "$(conda shell.bash hook)"
-conda activate my-rdkit-env_111_171_163
+conda activate my-rdkit-env
 
 PYARGS=""
 PYARGS="$PYARGS --name Ep0_rewFacNone_stochastic_kernel"
 PYARGS="$PYARGS --data_path src/dataset"
-PYARGS="$PYARGS --artifact_path aritfact"
+PYARGS="$PYARGS --artifact_path artifact"
 PYARGS="$PYARGS --gpu 0"
-PYARGS="$PYARGS --surrogate_model_path artifact/surrogate_model.pth"
+PYARGS="$PYARGS --surrogate_model_url https://portal.nersc.gov/project/m3623/docking_score_models/NSP15_6W01_A_1_F_uncharged_upsamp/predict_logp/best_model.pth"
 PYARGS="$PYARGS --surrogate_reward_timestep_delay 10"
 PYARGS="$PYARGS --prob_redux_factor 0.99"
 PYARGS="$PYARGS --stochastic_kernel"
