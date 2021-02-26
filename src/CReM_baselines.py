f94e6dd6081e0137df9df626dc3d063f5e2321ca
andrewdchen <andrewdchen@berkeley.edu>
2021-02-26 13:38:09 -0800

    two surrogate test

--- src/CReM_baselines.py
+++ src/CReM_baselines.py
@@ -31,7 +31,7 @@ from utils.general_utils import load_surrogate_model, maybe_download_file
 # Downloading files.
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 #surrogate_model_url = "https://portal.nersc.gov/project/m3623/docking_score_models/NSP15_6W01_A_1_F/20201210_170_161_baseline/best_model.pth"
-training_data_url = "https://portal.nersc.gov/project/m3623/MD/uncharged_unique/uncharged_NSP15_6W01_A_1_F.Orderable_zinc_db_enaHLL.2col.csv"
+training_data_url = "https://portal.nersc.gov/project/m3623/MD/uncharged_unique/NSP15_6W01_A_1_F_combined_sorted_negonly_unique1.csv"
 
 maybe_download_file(os.path.join(os.getcwd(), "train_data.csv"), training_data_url, "train_data")
 
@@ -40,7 +40,8 @@ maybe_download_file(os.path.join(os.getcwd(), "train_data.csv"), training_data_u
 
 
 # Loading Training Data and Surrogate Models
-model = load_surrogate_model("", "", "/global/home/users/adchen/exaLearnMol/surrogate_model/NSP15_6W01_A_1_F_uncharged_upsamp/predict_logp/best_model.pth", device)
+model = load_surrogate_model("", "", "/clusterfs/csdata/data/surrogate_models/NSP15_combined1_vanilla/predict_logp/best_model.pth", device)
+model_2 = load_surrogate_model("", "", "/clusterfs/csdata/data/surrogate_models/NSP15_combined2_vanilla/predict_logp/best_model.pth", device)
 
 with open(os.path.join(os.getcwd(), "train_data.csv"), 'r') as fp:
     reader = csv.reader(fp, delimiter=',', quotechar='"')
@@ -55,7 +56,7 @@ training_sample = random.sample(training_data, 1000)
 
 
 # Defining helper functions and threshold
-threshold = -17.42
+threshold = -20
 
 def get_scores(states, surrogate_model, device):
     g = Batch().from_data_list([mol_to_pyg_graph(state) for state in states])
@@ -70,6 +71,7 @@ def get_best(ms, model, device):
     # model - surrogate_model
     assert len(ms) > 0, "Molecule list empty"
     pred = get_scores(ms, model, device)
+    assert len(pred) > 0, "Predictions empty"
     i = np.argmin(pred)
     return ms[i], pred[i]
 
@@ -79,7 +81,7 @@ def evolve_mol(mol):
     while best_score > threshold and iter_num < 5:
         new_mols = list(mutate_mol(mol, db_name='../replacements02_sc2.db', return_mol=True, min_size=0, max_size=8, min_inc=-3, max_inc=3, ncores=8))
         new_mols = [Chem.RemoveHs(i[1]) for i in new_mols]
-        best_mol, score = get_best(new_mols + [mol], model, device)
+        best_mol, score = get_best(new_mols + [mol] + [mol], model, device)
         
         if score < best_score:
             best_score = score
@@ -113,18 +115,15 @@ def random_walk(mol):
 
 
 #Greedy search with for loop
-results = []
-for row in training_data:
-    mol = Chem.MolFromSmiles(row[0])
-    score = row[1]
-    best_mol, best_score = evolve_mol(mol)
-    #results.append((Chem.MolToSmiles(mol), score, Chem.MolToSmiles(best_mol), best_score ))
-    #mol = Chem.MolFromSmiles(row[0])
-    random_mol, random_score = random_walk(mol)
-    results.append((Chem.MolToSmiles(mol), score, Chem.MolToSmiles(random_mol), random_score, Chem.MolToSmiles(best_mol), best_score))
 
-file = open('baselines.csv', 'w+', newline = '')
-
+file = open('only_greedy_baselines.csv', 'w+', newline = '')
 with file:
     write = csv.writer(file)
-    write.writerows(results)
+    for row in training_sample:
+        mol = Chem.MolFromSmiles(row[0])
+        score = row[1]
+        best_mol, best_score = evolve_mol(mol)
+        surr2_score = get_scores([best_mol], model_2, device)
+        #random_mol, random_score = random_walk(mol)
+        write.writerow([Chem.MolToSmiles(mol), score, Chem.MolToSmiles(best_mol), best_score, surr2_score])
+
