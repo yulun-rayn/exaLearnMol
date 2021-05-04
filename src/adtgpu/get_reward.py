042257106b39b39e31528110eba0ce9ac97bf088
Yulun Rayn Wu <yulun_wu@berkeley.edu>
2021-05-04 11:05:53 -0700

    restructure

---
+++ src/adtgpu/get_reward.py
@@ -0,0 +1,38 @@
+import shutil
+import subprocess
+
+from rdkit import Chem
+
+def get_reward(states):
+    if not isinstance(states, list):
+        states = [states]
+
+    #print(states)
+    smiles = [Chem.MolToSmiles(mol) for mol in states]
+    #print(smiles)
+
+    #Print smiles to a file as input to ADT-gpu
+    adttmp="./src/adtgpu/autodockgpu"
+    if not os.path.exists(adttmp):
+        os.makedirs(adttmp)
+    if not os.path.exists(adttmp+"/ligands"):
+        os.makedirs(adttmp+"/ligands")
+    with open(adttmp+"/smiles.txt","w") as smiles_file:
+        smiles_file.writelines("%s\n" % sm for sm in smiles)
+
+    #Run ADT-gpu
+    #cmd="python /gpfs/alpine/syb105/proj-shared/Personal/mcashman/Projects/GCPN_MLDrugDiscovery/exaLearnMol_crem_parallel_GPU/scripts/run_adtgpu.py -r /gpfs/alpine/syb105/proj-shared/Projects/StructPred/Software/progs/adtgpu_sa/gcpn/scripts/test_run.Ver4/receptor/NSP15_6W01_A_1_F_receptor.pdb -s \"" + str(smiles) + "\" -d autodockgpu"    
+    cmd="python ./src/adtgpu/run_adtgpu.py -r ./src/adtgpu/receptor/NSP15_6W01_A_1_F_receptor.pdbqt -s \"" + str(smiles) + "\" -d "+adttmp    
+    subprocess.Popen(cmd,shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
+
+    #Fetch output scores
+    pred_docking_score=[]
+    with open(adttmp+"/adt_affinity_vals","r") as scores:
+        for mol in scores.readlines():
+            pred_docking_score.append(-float(mol.strip()))
+    #print("\npred_docking_score:\n{}".format(pred_docking_score))
+
+    shutil.rmtree(adttmp)
+    print("Reward Scores (-dock): {}".format(pred_docking_score))
+    return (pred_docking_score)
+
--- src/gcpn/PPO.py
+++ src/gcpn/PPO.py
@@ -1,6 +1,4 @@
 import os
-import shutil
-import subprocess
 import sys
 import gym
 import copy
@@ -23,7 +21,8 @@ from utils.graph_utils import mol_to_pyg_graph, get_batch_shift
 
 from gnn_surrogate.model import GNN_MyGAT
 
-from rdkit import Chem
+from adtgpu.get_reward import get_reward
+
 
 class Memory:
     def __init__(self):
@@ -255,46 +254,6 @@ class PPO_GCPN(nn.Module):
 
 
 #####################################################
-#                   FINAL REWARDS                   #
-#####################################################
-
-def get_reward(states, surrogate_model, device, done_idx=None):
-    if not isinstance(states, list):
-        states = [states]
-    if done_idx is None:
-        done_idx = range(len(states))
-
-    #print(states)
-    smiles = [Chem.MolToSmiles(mol) for mol in states]
-    #print(smiles)
-
-    #Print smiles to a file as input to ADT-gpu
-    adttmp="./src/adtgpu/autodockgpu"
-    if not os.path.exists(adttmp):
-        os.makedirs(adttmp)
-    if not os.path.exists(adttmp+"/ligands"):
-        os.makedirs(adttmp+"/ligands")
-    with open(adttmp+"/smiles.txt","w") as smiles_file:
-        smiles_file.writelines("%s\n" % sm for sm in smiles)
-
-    #Run ADT-gpu
-    #cmd="python /gpfs/alpine/syb105/proj-shared/Personal/mcashman/Projects/GCPN_MLDrugDiscovery/exaLearnMol_crem_parallel_GPU/scripts/run_adtgpu.py -r /gpfs/alpine/syb105/proj-shared/Projects/StructPred/Software/progs/adtgpu_sa/gcpn/scripts/test_run.Ver4/receptor/NSP15_6W01_A_1_F_receptor.pdb -s \"" + str(smiles) + "\" -d autodockgpu"    
-    cmd="python ./src/adtgpu/run_adtgpu.py -r ./src/adtgpu/receptor/NSP15_6W01_A_1_F_receptor.pdbqt -s \"" + str(smiles) + "\" -d "+adttmp    
-    subprocess.Popen(cmd,shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
-
-    #Fetch output scores
-    pred_docking_score=[]
-    with open(adttmp+"/adt_affinity_vals","r") as scores:
-        for mol in scores.readlines():
-            pred_docking_score.append(-float(mol.strip()))
-    #print("\npred_docking_score:\n{}".format(pred_docking_score))
-
-    shutil.rmtree(adttmp)
-    print("Reward Scores (-dock): {}".format(pred_docking_score))
-    return (pred_docking_score)
-
-
-#####################################################
 #                   TRAINING LOOP                   #
 #####################################################
 
@@ -502,9 +461,7 @@ def train_ppo(args, surrogate_model, env):
             nowdone_idx = [idx for idx in notdone_idx if idx in new_done_idx]
             stillnotdone_idx = [idx for idx in notdone_idx if idx in new_notdone_idx]
             if len(nowdone_idx) > 0:
-                surr_rewards = get_reward(
-                    [mols[idx] for idx in nowdone_idx],
-                    surrogate_model, device)
+                surr_rewards = get_reward([mols[idx] for idx in nowdone_idx])
 
             for i, idx in enumerate(nowdone_idx):
                 try:
