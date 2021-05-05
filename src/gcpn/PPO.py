0c6826bca9381c423bf2bbde72e6b37aa1dab47d
Yulun Rayn Wu <yulun_wu@berkeley.edu>
2021-05-04 19:13:26 -0700

    fix reporting part2

--- src/gcpn/PPO.py
+++ src/gcpn/PPO.py
@@ -514,7 +514,7 @@ def train_ppo(args, surrogate_model, env):
         # save every 500 episodes
         if save_counter >= save_interval:
             torch.save(ppo.policy.actor, os.path.join(save_dir, '{:05d}_gcpn.pth'.format(i_episode)))
-            save_counter -= save_interval
+            save_counter = 0
         
         # save running model
         torch.save(ppo.policy.actor, os.path.join(save_dir, 'running_gcpn.pth'))
@@ -526,7 +526,7 @@ def train_ppo(args, surrogate_model, env):
             print('Episode {} \t Avg length: {} \t Avg reward: {:5.3f}'.format(i_episode, avg_length, running_reward))
             running_reward = 0
             avg_length = 0
-            log_counter -= log_interval
+            log_counter = 0
 
         episode_count = 0
         sample_count = 0
