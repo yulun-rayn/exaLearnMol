f8f2b6761745b9e4f1812ed85e98808dcf76952f
Yulun Rayn Wu <yulun_wu@berkeley.edu>
2021-05-05 22:41:18 -0700

    clean up

--- src/logp/get_reward.py
+++
@@ -1,8 +0,0 @@
-from rdkit.Chem.Descriptors import MolLogP
-
-def get_logp_scores(states):
-    if not isinstance(states, list):
-        states = [states]
-    scores = [MolLogP(state) for state in states]
-    return scores
-    
