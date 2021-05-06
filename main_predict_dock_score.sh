0ebd9995e59ccc26ed0396cf6af89fdc5431d8ac
Yulun Rayn Wu <yulun_wu@berkeley.edu>
2021-05-06 11:43:45 -0700

    A3H

--- main_predict_dock_score.sh
+++ main_predict_dock_score.sh
@@ -6,9 +6,9 @@ conda activate my-rdkit-new
 DATA=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 PYARGS=""
-PYARGS="$PYARGS --name predict_dock_default"
-PYARGS="$PYARGS --data_path $DATA/src/dataset/NSP15_6W01_A_1_F.Orderable_zinc_db_enaHLL.sorted.4col.csv"
-PYARGS="$PYARGS --artifact_path $DATA/artifact/gcpn"
+PYARGS="$PYARGS --name predict_dock_crem_parallel_A3H"
+PYARGS="$PYARGS --data_path $DATA/src/dataset/NSP15_6W01_A_3_H.negonly_unique.csv"
+PYARGS="$PYARGS --artifact_path $DATA/artifact/dgapn"
 # PYARGS="$PYARGS --use_3d"
 
 python src/main_predict_dock_score.py $PYARGS
